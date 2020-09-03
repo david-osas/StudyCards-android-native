9f75ffcb2667fe1cca5412b7099d6531e81fc431
david-osas <azamegbeosarumense@gmail.com>
2020-09-03 19:13:59 +0100

    Added initial changes create deck UI

--- app/src/main/java/com/example/studycards/CreateDeckActivity.java
+++ app/src/main/java/com/example/studycards/CreateDeckActivity.java
@@ -3,12 +3,34 @@ package com.example.studycards;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.studycards.databinding.ActivityCreateDeckBinding;
 
 public class CreateDeckActivity extends AppCompatActivity {
+
+    private ActivityCreateDeckBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_deck);
+        binding = ActivityCreateDeckBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
     }
+
+    public void switchQuestion(View view){
+        binding.questionButton.setBackgroundResource(R.color.colorAccent);
+        binding.answerButton.setBackgroundResource(R.color.button_dull);
+        binding.details.setHint(R.string.card_question);
+    }
+
+    public void switchAnswer(View view){
+        binding.answerButton.setBackgroundResource(R.color.colorAccent);
+        binding.questionButton.setBackgroundResource(R.color.button_dull);
+        binding.details.setHint(R.string.card_answer);
+    }
+
+
 }
